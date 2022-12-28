3a0b44febd971c9e03b248ab95035a8246ee5a04
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2022-12-28 14:16:48 +0100

    inint project

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("hi")
+import myjson from "./data/data.json" assert {type:"json"}
+import express from 'express'
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    console.log("here")
+
+    res.json(myjson)
+   
+})
+app.listen(3000)
