b23b00ac7f4af28786f0cd62ed6b85fe1291ba7e
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-02 11:57:25 +0100

    auth for dbs

--- routes/dbs.js
+++ routes/dbs.js
@@ -1,32 +1,43 @@
-
 const express = require("express");
 const router = express.Router();
 const Book = require("../models/book");
+const BookDetails = require("../models/book-details");
 const Shelf = require("../models/shelf");
+const User = require("../models/user");
+
+const passport = require("passport");
+const initializePassport = require("../passportConfig");
+initializePassport(passport);
 
 // Delete one
-router.delete("/:id", async (req, res) => {
+router.delete("/:id",  passport.authenticate("jwt", { session: false }), async (req, res) => {
   try {
     switch (req.params.id) {
-      case 'books':
+      case "books":
         await Book.collection.drop();
         res.json({ message: "Deleted books collection" });
         break;
-      case 'shelves':
+      case "shelves":
         await Shelf.collection.drop();
         res.json({ message: "Deleted shelves collection" });
         break;
+      case "bookdetails":
+        await BookDetails.collection.drop();
+        res.json({ message: "Deleted bookDetailss collection" });
+        break;
+      case "users":
+        await User.collection.drop();
+        res.json({ message: "Deleted users collection" });
+        break;
       default:
-        res.status(404).json({ message: 'collection not found' });
+        res.status(404).json({ message: "collection not found" });
     }
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
-
 module.exports = router;
-
 
 // let cmd = `mongodump --forceTableScan --out=${backupDirPath} --uri=${process.env.MONGODB_URI}`;
 
