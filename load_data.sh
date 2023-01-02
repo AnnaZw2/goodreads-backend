d3f7c60478aefdd0db10c4f2874c1c98ab579d29
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-02 11:58:08 +0100

    load data with auth

--- load_data.sh
+++ load_data.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # start server node server.js - not nodemon server.js (too fast requests)
 export SLEEP=0.1
+export ADMIN_JWT="Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoiYWRtaW5AbG9jYWxob3N0Iiwicm9sZSI6ImFkbWluIn0sImlhdCI6MTY3MjYxMzA2OSwiZXhwIjoxNjczMjE3ODY5fQ.YS_IK7RUfPdpVow4Kpuc-TlnNhABZw10oF96uNeBONc"
+
 rm -rf load_data_scratch
 mkdir -p load_data_scratch/{books,shelves,details}
 cd  load_data_scratch/books
 jq -c  '.[]' ../../data/books.json | awk '{print > "doc00" NR ".json";}'
-for FILE in *.json; do curl -i -X POST -H "Content-type: application/json" -d @$FILE http://localhost:3000/books; sleep $SLEEP; done
+for FILE in *.json; do curl -i -X POST -H "$ADMIN_JWT" -H "Content-type: application/json" -d @$FILE http://localhost:3000/books; sleep $SLEEP; done
 
 cd ../shelves
 jq -c  '.[]' ../../data/shelves.json | awk '{print > "doc00" NR ".json";}'
-for FILE in *.json; do curl -i -X POST -H "Content-type: application/json" -d @$FILE http://localhost:3000/shelves; sleep $SLEEP;  done
+for FILE in *.json; do curl -i -X POST -H "$ADMIN_JWT" -H "Content-type: application/json" -d @$FILE http://localhost:3000/shelves; sleep $SLEEP;  done
 
 cd ../..
 node ./prepare_details.js > load_data_scratch/details/details.txt
@@ -17,4 +19,4 @@ cd load_data_scratch/details
 export BOOKS=`curl -s localhost:3000/books | jq '.[] | ._id' |  tr -d '"'`
 export SHELVES=`curl -s localhost:3000/shelves | jq '.[] | ._id' |  tr -d '"'`
 jq -c  '.[]' details.txt | awk '{print > "doc00" NR ".json";}'
-for FILE in *.json; do curl -i -X POST -H "Content-type: application/json" -d @$FILE http://localhost:3000/book-details; sleep $SLEEP;  done
+for FILE in *.json; do curl -i -X POST -H "$ADMIN_JWT" -H "Content-type: application/json" -d @$FILE http://localhost:3000/book-details; sleep $SLEEP;  done
