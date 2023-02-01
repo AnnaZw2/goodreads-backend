6ca53ff7c898f3bb8e064ae70f1b5ff1af74c33f
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-02-01 18:46:01 +0100

    reformat

--- src/workers/account-watcher/account-watcher.js
+++ src/workers/account-watcher/account-watcher.js
@@ -1,14 +1,17 @@
 require('dotenv-defaults').config()
 
-const mqttClient  = require('./mqtt')
-mqttClient.on('message',function(topic, message, packet){
-// do some stuff with the message
-const msg = JSON.parse(message);
-if (msg.username == undefined || msg.username == null) {
-    msg.username = "not set";
-} 
-mqttClient.publish(process.env.MQTT_TOPIC_PREFIX+"slack/send", `Account created: username: ${msg.username}, email: ${msg.email}, role: ${msg.role}`, {qos: 1});
+const mqttClient = require('./mqtt')
+mqttClient.subscribe(process.env.MQTT_TOPIC_PREFIX + "user/created", { qos: 1 });
+
+mqttClient.on('message', function (topic, message, packet) {
+    // do some stuff with the message
+    console.log("Received message: " + message);
+    const msg = JSON.parse(message);
+    if (msg.username == undefined || msg.username == null) {
+        msg.username = "not set";
+    }
+    mqttClient.publish(process.env.MQTT_TOPIC_PREFIX + "slack/send", `Account created: username: ${msg.username}, email: ${msg.email}, role: ${msg.role}`, { qos: 1 });
+   
 });
 
-mqttClient.subscribe(process.env.MQTT_TOPIC_PREFIX+"user/created", {qos: 1});
 
