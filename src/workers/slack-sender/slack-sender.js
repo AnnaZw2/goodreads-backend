bf664e2c88e20ac8be4f5c10ffd8a81343aaa96f
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-02-01 18:44:25 +0100

    tidy slack-sender

--- src/workers/slack-sender/slack-sender.js
+++ src/workers/slack-sender/slack-sender.js
@@ -2,15 +2,14 @@ require('dotenv-defaults').config()
 
 const slackNotify = require('slack-notify');
 const slack = slackNotify(process.env.SLACK_WEBHOOK_URL);
+const mqttClient = require('./mqtt')
 
-const mqttClient  = require('./mqtt')
-mqttClient.on('message',function(topic, message, packet){
-    slack.send({
-        channel: '#goodreads',
-        icon_emoji: ':ghost:',
-        text: message.toString(),
-        unfurl_links: 1,
-        username: 'Admin'
-      });
+mqttClient.subscribe(process.env.MQTT_TOPIC_PREFIX + "slack/send", { qos: 1 });
+
+mqttClient.on('message', function (topic, message, packet) {
+
+  slack.send({
+    text: message.toString(),
+
+  });
 });
-mqttClient.subscribe(process.env.MQTT_TOPIC_PREFIX+"slack/send", {qos: 1});
